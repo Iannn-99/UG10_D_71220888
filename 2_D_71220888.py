f63b5d1ba6d2915bc91241496724f47fd9cbc802
Iannn-99 <117901330+Iannn-99@users.noreply.github.com>
2022-11-24 16:16:26 +0700

    Add files via upload

--- 2_D_71220888.py
+++ 2_D_71220888.py
@@ -4,7 +4,7 @@ nama= input("Masukkan nama pemain ketiga: ")
 c1=int(input("Masukkan jumlah kartu pemain pertama: "))
 c2=int(input("Masukkan jumlah kartu pemain kedua: "))
 c3=int(input("Masukkan jumlah kartu pemain ketiga: "))
-if (c):
-    print("{} menang dengan jumlah kartu sebanyak {}" .format(nama,c1 or c2 or c3))
+if (c2<=21):
+    print("{} menang dengan jumlah kartu sebanyak {}" .format(nama,c2))
 else:
-    print()
+    print("Jumlah kartu yang dimiliki melebihi batas")
